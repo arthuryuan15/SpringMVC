c1949ccd0b0c21efa1b10f9912efccb4fde6c02e
YUANJINSHUAI <arthuryuan15@outlook.com>
2020-04-19 10:18:12 +0800

    add some note in HelloServlet.java

--- springmvc-01-serlvet/src/main/java/com/yuan/servlet/HelloServlet.java
+++ springmvc-01-serlvet/src/main/java/com/yuan/servlet/HelloServlet.java
@@ -11,17 +11,17 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 1. 获取前端参数
-        String method = req.getParameter("method");
+        String method = req.getParameter("method"); // url映射到java类或java类的方法
         if(method.equals("add")){
-            req.getSession().setAttribute("msg","执行了add方法");
+            req.getSession().setAttribute("msg","执行了add方法"); // 封装用户提交的数据 .
         }
         if(method.equals("delete")){
-            req.getSession().setAttribute("msg","执行了delete方法");
+            req.getSession().setAttribute("msg","执行了delete方法"); // 封装用户提交的数据 .
         }
         // 2. 调用业务层
 
         // 3. 视图层转发或者重定向
-        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req,resp); // 将响应的数据进行渲染 . jsp / html 等表示层数据
 
     }
 
